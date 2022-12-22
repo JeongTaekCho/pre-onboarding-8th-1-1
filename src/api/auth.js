3f606c2e585759840293638d2735904fd84376e0
munchking98 <blood89@nate.com>
2022-12-22 23:53:25 +0900

    🐛 Fix :  누락된 에러처리 추가

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,33 +1,20 @@
-import axios from 'axios';
 import client from '../utils/httpClient';
 
-const SERVER_URL = process.env.REACT_APP_SERVER_URL;
-const headers = { headers: { 'Content-Type': 'application/json' } };
-export const postSignup = async (data) => {
-  try {
-    const res = await axios.post(`${SERVER_URL}/auth/signup`, data, headers);
-    if (res) localStorage.setItem('access_token', `Bearer ${res.data.access_token}`);
-    return res;
-  } catch (err) {
-    return err;
-  }
+export const authAPI = {
+  signup: async (userInfo) => {
+    try {
+      const res = await client.post('/auth/signup', userInfo);
+      return res;
+    } catch (err) {
+      return err;
+    }
+  },
+  signin: async (userInfo) => {
+    try {
+      const res = await client.post('/auth/signin', userInfo);
+      return res;
+    } catch (err) {
+      return err;
+    }
+  },
 };
-
-export const postLogin = async (data) => {
-  try {
-    const res = await axios.post(`${SERVER_URL}/auth/signin`, data, headers);
-    if (res) localStorage.setItem('access_token', `Bearer ${res.data.access_token}`);
-    return res;
-  } catch (err) {
-    return err;
-  }
-};
-
-// export const authAPI = {
-//   signup: (email, password) => {
-//     return client.post('/auth/signup', { email, password });
-//   },
-//   signin: (email, password) => {
-//     return client.post('/auth/signin', { email, password });
-//   },
-// };
