2cf5af405a6dc87ffb0e623929bfb869c11adb02
JeongTaekCho <cjt3591@gmail.com>
2022-12-23 12:41:01 +0900

    💄 Design:  투두 리스트 등록/수정/삭제 버튼 디자인 수정 및 모달 추가

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Modal from './Modal';
 
 const TodoInputComponent = styled.form`
   width: 100%;
@@ -8,46 +9,75 @@ const TodoInputComponent = styled.form`
   border: 1px solid #eee;
   border-radius: 10px;
   margin: 24px 0;
+  align-items: center;
+  padding: 16px;
+  box-sizing: border-box;
 
   input {
     width: 100%;
-    padding: 8px;
     font-size: 16px;
     font-weight: 700;
-    margin: 16px;
     border: none;
     background: #fff;
     color: #202020;
+    outline: none;
+    padding: 8px 0;
   }
 
   button {
-    all: unset;
-    width: 80px;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    background-color: green;
+    font-size: 18px;
+    color: #fff;
     display: flex;
     justify-content: center;
     align-items: center;
     cursor: pointer;
     font-weight: 700;
-    border-top-right-radius: 10px;
-    border-bottom-right-radius: 10px;
+    border: none;
+    :active {
+      opacity: 0.8;
+    }
   }
 `;
 
 const TodoInput = ({ handleCreateTodo }) => {
   const [todo, setTodo] = useState('');
+  const [isModal, setIsModal] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const handleIsModal = () => setIsModal(!isModal);
 
   const handleChangeInput = (e) => setTodo(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleCreateTodo(todo);
-    setTodo('');
+    if (todo) {
+      handleCreateTodo(todo);
+      setIsSuccess(true);
+      setIsModal(true);
+      setTodo('');
+    } else {
+      setIsSuccess(false);
+      setIsModal(true);
+    }
   };
   return (
-    <TodoInputComponent onSubmit={handleSubmit}>
-      <input placeholder="new Todo.." onChange={handleChangeInput} value={todo} />
-      <button type="submit">+</button>
-    </TodoInputComponent>
+    <>
+      <TodoInputComponent onSubmit={handleSubmit}>
+        <input placeholder="new Todo.." onChange={handleChangeInput} value={todo} />
+        <button type="submit">+</button>
+      </TodoInputComponent>
+      {isModal && (
+        <Modal
+          title={isSuccess ? '성공' : '실패'}
+          content={isSuccess ? '할 일이 성공적으로 등록되었습니다!✨' : '등록에 실패했습니다. 다시 확인해주세요.🥲'}
+          onClose={handleIsModal}
+        />
+      )}
+    </>
   );
 };
 
